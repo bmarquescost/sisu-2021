cdb00150fa1d6e98ecf600c5f519c6b7757706c4
Bernardo Marques <b.marquescost@gmail.com>
2021-04-10 16:20:03 -0300

    Adicionado mudanças nas requisições e diretorio com as parciais

--- notas_corte_request.py
+++ notas_corte_request.py
@@ -1,19 +1,20 @@
 import requests
 import pandas as pd
+from time import sleep
 
 # A cada parcial, faremos requisições para o site obtenod informações sobre cada nota de corte
 # Faremos requisições para o site https://sisu-api-pcr.apps.mec.gov.br/api/v1/oferta/{}/modalidades em que {} é substituido pelo código do oferecimento
 try:
-    dataframe_cursos = pd.read_csv('lista_cursos.csv', sep= ';')
+    dataframe_cursos = pd.read_csv("lista_cursos.csv", sep= ";")
 except:
     print("Necessário gerar arquivo lista_cursos.csv\nPara isso, rode o programa lista_cursos.request.py")
     exit()
 
-arquivo_final_csv = input('Digite o nome do arquivo em será guardado os dados finais (sem extensão): ')
+arquivo_final_csv = input("Digite o nome do arquivo em será guardado os dados finais (sem extensão): ")
 
-url_modalidades = 'https://sisu-api-pcr.apps.mec.gov.br/api/v1/oferta/{}/modalidades'
+url_modalidades = "https://sisu-api-pcr.apps.mec.gov.br/api/v1/oferta/{}/modalidades"
 
-codigos_oferta = dataframe_cursos['codigo_oferta']
+codigos_oferta = dataframe_cursos["codigo_oferta"]
 
 dataframe_modalidades = pd.DataFrame()
 for index, codigo_oferta in enumerate(codigos_oferta):
@@ -25,23 +26,29 @@ for index, codigo_oferta in enumerate(codigos_oferta):
         except:
             print(f"[{codigo_oferta}] Erro ocorreu na requisição, tentando novamente")
     
+    while resposta.status_code != 200:
+        print(f"[{codigo_ofera}] Erro {resposta.status_code}")
+        resposta = requests.get(url_modalidades.format(codigo_oferta))
+        sleep(1)
+        continue
+
     dados_modalidades = resposta.json()
 
     print(f"[{index + 1} / {codigos_oferta.shape[0]}] {dataframe_cursos.loc[index, 'nome_ies']} ({dataframe_cursos.loc[index, 'sigla_ies']}) {dataframe_cursos.loc[index, 'nome_curso']}")
     
-    for modalidade in dados_modalidades['modalidades']:
+    for modalidade in dados_modalidades["modalidades"]:
             
         dados = {
-            'codigo_oferta': codigo_oferta,
-            'codigo_concorrencia': modalidade['co_concorrencia'],
-            'quantidade_vagas': modalidade['qt_vagas'],
-            'nota_corte': modalidade['nu_nota_corte'],
-            'bonus_percentual': modalidade['qt_bonus_perc'],
-            'data_nota_corte': modalidade['dt_nota_corte']
+            "codigo_oferta": codigo_oferta,
+            "codigo_concorrencia": modalidade["co_concorrencia"],
+            "quantidade_vagas": modalidade["qt_vagas"],
+            "nota_corte": modalidade["nu_nota_corte"],
+            "bonus_percentual": modalidade["qt_bonus_perc"],
+            "data_nota_corte": modalidade["dt_nota_corte"]
         }
         for column in dataframe_cursos.columns.tolist():
             dados[column] = dataframe_cursos.loc[index, column]
 
         dataframe_modalidades = dataframe_modalidades.append(dados, ignore_index=True)
     
-dataframe_modalidades.to_csv(arquivo_final_csv + '.csv', sep=';', index=False)
+dataframe_modalidades.to_csv("/Data/" + arquivo_final_csv + ".csv", sep=";", index=False)
